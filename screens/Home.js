cff2580295d7f736a09067f9cacb42d7235a0b7d
Sudharsan <sudharsanvsr@gmail.com>
2022-12-15 10:54:08 +0530

    Navigation Added.

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                Hello World!
+            </Text>
+        </View>
+    );
+};
+
+export default Home;
